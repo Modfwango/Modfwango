822e2d44a49b0052a30d9306a6a1460306b7b8f0
Clay Freeman <git@clayfreeman.com>
2014-07-08 21:43:00 -0500

    Why?

--- includes/connectionManagement.php
+++ includes/connectionManagement.php
@@ -90,7 +90,7 @@
     public static function loadConnectionFile($file, $connect = false) {
       // Parse the files using an ini parser.
       $connection = parse_ini_file($file, true);
-      Logger::debug(str_ireplace("\n", " ", var_export($connection, true)));
+      Logger::debug(var_export($connection, true));
 
       // Require these items to be defined.
       if (isset($connection['address']) && isset($connection['port'])) {
