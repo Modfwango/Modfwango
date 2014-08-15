309a980f8d8b35934337ed023862dc77de2bc410
Clay Freeman <git@clayfreeman.com>
2014-08-14 23:35:28 -0500

    Test with fread.

--- includes/connection.php
+++ includes/connection.php
@@ -181,7 +181,7 @@
       // Check to make sure the socket is a valid resource.
       if (is_resource($this->socket)) {
         // Attempt to read data from the socket.
-        if ($data = @fgets($this->socket, 8192)) {
+        if ($data = @fread($this->socket, 8192)) {
           if ($data != false && strlen($data) > 0) {
             // Return the data.
             Logger::debug("Data received on '".$this->getConnectionString().
