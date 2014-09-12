f622423dc44ec09a44f23766b504387ff8b73515
Clay Freeman <git@clayfreeman.com>
2014-09-12 16:07:31 -0500

    Check to make sure that the connection is IPC.

--- modules/events/IPCRawEvent.php
+++ modules/events/IPCRawEvent.php
@@ -3,12 +3,14 @@
     public $name = "IPCRawEvent";
 
     public function preprocessEvent($name, $registrations, $connection, $data) {
-      $ex = explode(" ", $data);
+      if ($connection->getIPC() == true) {
+        $ex = explode(" ", $data);
 
-      // Iterate through each registration.
-      foreach ($registrations as $id => $registration) {
-        // Trigger the event for a certain registration.
-        EventHandling::triggerEvent($name, $id, array($connection, $data, $ex));
+        // Iterate through each registration.
+        foreach ($registrations as $id => $registration) {
+          // Trigger the event for a certain registration.
+          EventHandling::triggerEvent($name, $id, array($connection, $data, $ex));
+        }
       }
     }
 
