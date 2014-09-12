f076548211b0bad8768329353ea529ea0c22aeef
Clay Freeman <git@clayfreeman.com>
2014-09-12 15:46:33 -0500

    More testing.

--- includes/IPCHandling.php
+++ includes/IPCHandling.php
@@ -1,7 +1,40 @@
 <?php
   class IPCHandling {
+    public static function dispatch($module, $method, $callback, $data = null) {
+      $pid = pcntl_fork();
+      if ($pid == -1) {
+        die('could not fork');
+      } else if ($pid) {
+        //parent
+        return $pid;
+      } else {
+        //child
+        foreach (SocketManagement::getSockets() as $socket) {
+          if ($socket->getIPC() == true) {
+            $connection = new Connection("0", array(
+              $socket->getHost(),
+              $socket->getPort(),
+              $socket->getSSL(),
+              array()
+            ));
+            $connection->send(json_encode($module->$method($data)));
+            $connection->disconnect();
+          }
+        }
+      }
+    }
+
     public static function receiveData($connection, $data) {
-      Logger::info(var_export($data, true));
+      // Get the IPCRawEvent event.
+      $event = EventHandling::getEventByName("IPCRawEvent");
+      if ($event != false) {
+        foreach ($event[2] as $id => $registration) {
+          // Trigger the IPCRawEvent event for each registered
+          // module.
+          EventHandling::triggerEvent("IPCRawEvent", $id,
+            array($connection, $data));
+        }
+      }
     }
   }
 ?>
---
+++ modules/events/IPCRawEvent.php
@@ -0,0 +1,21 @@
+<?php
+  class __CLASSNAME__ {
+    public $name = "IPCRawEvent";
+
+    public function preprocessEvent($name, $registrations, $connection, $data) {
+      $ex = explode(" ", $data);
+
+      // Iterate through each registration.
+      foreach ($registrations as $id => $registration) {
+        // Trigger the event for a certain registration.
+        EventHandling::triggerEvent($name, $id, array($connection, $data, $ex));
+      }
+    }
+
+    public function isInstantiated() {
+      // Create an event for raw data.
+      EventHandling::createEvent("IPCRawEvent", $this, "preprocessEvent");
+      return true;
+    }
+  }
+?>
