f05b4298b24138c8d343cf2cbb93d38fafeab3ee
Clay Freeman <git@clayfreeman.com>
2014-09-12 19:58:58 -0500

    Negate unnecessary change for pull request.

--- modules/events/RawEvent.php
+++ modules/events/RawEvent.php
@@ -8,8 +8,7 @@
       // Iterate through each registration.
       foreach ($registrations as $id => $registration) {
         // Trigger the event for a certain registration.
-        EventHandling::triggerEvent($name, $id, array($connection, $data,
-          $ex));
+        EventHandling::triggerEvent($name, $id, array($connection, $data, $ex));
       }
     }
 
