543de4fa246218ca0b316d80027a967555b2d52a
Clay Freeman <git@clayfreeman.com>
2015-10-12 22:05:10 -0500

    Fixed?

--- includes/process.php
+++ includes/process.php
@@ -85,8 +85,9 @@
       // Check to make sure the process is a valid resource
       if (is_resource($this->process)) {
         // Attempt to read data from the process
-        if ($data = @fread(($err ? $this->err : $this->in), 8192)) {
-          if ($data != false) {
+        if ($data = @stream_get_line(($err ? $this->err : $this->in),
+            8192, "\n")) {
+          if ($data !== false) {
             // Sanitize data
             $data = trim($data);
             // Return the data
