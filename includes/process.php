2a1c9747f0f7a91a25c79bd5d07e75fe42628453
Clay Freeman <git@clayfreeman.com>
2015-10-20 22:10:12 -0500

    Revert the same.

--- includes/process.php
+++ includes/process.php
@@ -98,7 +98,7 @@
       // Check to make sure the process is a valid resource
       if (is_resource($fd)) {
         // Attempt to read data from the process
-        if ($data = @fgets($fd, 262144)) {
+        if ($data = @stream_get_line($fd, 8192, "\n")) {
           // Sanitize data
           $data = trim($data);
           // Return the data
